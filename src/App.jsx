23204bdd2856ed22b82c2ca84ea5ace1323476f7
Mohammed Shirhaan <mshirhaan@gmail.com>
2024-07-19 21:13:30 +0530

    demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
+import { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
 
+window.ganesh = [];
 function App() {
-  return <div>Hello Geekster</div>;
+  const [firstName, setFirstName] = useState("");
+  const [secondName, setSecondName] = useState("");
+
+  const [age, setAge] = useState(0);
+
+  const firstNameRef = useRef(null);
+  const secondNameRef = useRef(null);
+
+  // function printName() {
+  //   return `Mr. ${firstName} ${secondName}`;
+  // }
+
+  const printName = useCallback(() => {
+    return `Mr. ${firstName} ${secondName}`;
+  }, [firstName, secondName]);
+
+  window.ganesh.push(printName);
+
+  return (
+    <div>
+      <input type="text" placeholder="first name" ref={firstNameRef} />
+
+      <input type="text" placeholder="second name" ref={secondNameRef} />
+
+      <input
+        type="number"
+        placeholder="age"
+        onChange={(e) => setAge(e.target.value)}
+      />
+
+      <button
+        onClick={() => {
+          setFirstName(firstNameRef.current.value);
+          setSecondName(secondNameRef.current.value);
+        }}
+      >
+        Save
+      </button>
+
+      <p>{printName()}</p>
+
+      <p>Age: {age}</p>
+    </div>
+  );
 }
 
 export default App;
